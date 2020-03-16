4ce14c18b429fa8fb0b4b2eb591eed2162461ae7
g.salvio <giuseppe.salvio@finconsgroup.com>
2020-03-16 17:15:31 +0100

    initial commit

--- guest-app/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO USER (USERNAME, PASSWORD) VALUES ('fpmoles', '$2a$11$dp4wMyuqYE3KSwIyQmWZFeUb7jCsHAdk7ZhFc0qGw6i5J124imQBi');--password haschcoded 11 times
-INSERT INTO USER (USERNAME, PASSWORD) VALUES ('jdoe', '$2a$11$3NO32OV1TGjap3xMpAEjmuiizitWuaSwUYz42aMtlxRliwJ8zm4Sm');--foobar haschcoded 11 times
